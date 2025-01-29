3aa4ef60df5787aa67fdc3e91c7fc074f4f61b14
Dev Mittal <154676624+devmittal420@users.noreply.github.com>
2025-01-29 16:25:58 +0530

    changes

--- src/Services/http/http.config.js
+++ src/Services/http/http.config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://65.1.167.168:4000/";
+// export const BASE_URL = "http://65.1.167.168:4000/";  // when aws running so use this
+export const BASE_URL = "http://localhost:4000/"; // when aws is not running so use this and run project on chrome using cors extension in chrome
 export const API_TIME_OUT = 15000;
 export const PRODUCT_URL = "products";
 export const CART_URL = "cart";
